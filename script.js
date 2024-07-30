7fb4779c54eeeaab934885a84b464365c36f7c34
Priyadharshini <priyadharshinik1720@gmail.com>
2024-07-30 19:18:28 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function submit(){
+    const pan=document.getElementById("pan").value;
+
+    if(pan==="")
+        alert("Enter your PAN number");
+    else if(pan.length!==10)
+        alert("Invalid PAN number");
+    else if(pan==="/^[A-Z]{5}[0-9]{4}[A-Z]{1}")
+        alert("Invalid PAN number");
+    else{
+        document.getElementById("result").innerHTML="Valid PAN Number";
+        pan.value="";
+    }
+}
